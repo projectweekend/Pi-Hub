74afc9af09301997eea34e9806d0bea50de69836
Brian Hines <brian@projectweekend.net>
2015-06-03 05:47:15 -0500

    Let's take a different approach here

--- handlers/index.js
+++
@@ -1,7 +0,0 @@
-var Handler = require( "../socket" ).Handler;
-
-
-exports.test = new Handler( "test" );
-exports.test.addTask( "test", function ( m ) {
-    console.log( m );
-} );
--- server.js
+++
@@ -1,8 +0,0 @@
-var io = require( "socket.io" )( process.env.PORT || 3030 );
-var _ = require( "lodash" );
-var handlers = require( "./handlers" );
-
-
-_.forOwn( handlers, function ( v, k ) {
-    v.registerChannel( io );
-} );
